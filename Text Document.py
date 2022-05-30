c38e39515d28d5ed2f4bc66eb55a98e43f7ea65e
FaLnA1203 <95920189+FaLnA1203@users.noreply.github.com>
2022-05-30 19:42:48 +0300

    xx

--- Text Document.py
+++
@@ -1,8 +0,0 @@
-import pygame
-from pygame.locals import *
-from pygame import mixer
-import pickle
-from os import path
-pickle_in = open('level0_data', 'rb')
-world_data = pickle.load(pickle_in)
-print(world_data)
